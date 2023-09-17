b90b35b059f2e0a5ed2ef986a3edf7df7ffce723
CorentinRuiz <corentin.r2126@gmail.com>
2023-09-17 20:05:54 +0200

    Add function to call menus service

---
+++ front/chez-philou/src/api/menus.js
@@ -0,0 +1,23 @@
+import { axiosInstance } from "./api";
+
+const API_BASE_ROUTE = "/menus";
+
+export const getMenus = async () => {
+    return await axiosInstance.get(API_BASE_ROUTE);
+};
+
+export const addMenuItem = async (fullName, shortName, price, category, image) => {
+    const body = {
+        fullName: fullName,
+        shortName: shortName,
+        price: price,
+        category: category,
+        image: image
+    };
+
+    return await axiosInstance.post(API_BASE_ROUTE, body);
+};
+
+export const getTableInformation = async (menuItemId) => {
+    return await axiosInstance.get(`${API_BASE_ROUTE}/${menuItemId}`);
+}
