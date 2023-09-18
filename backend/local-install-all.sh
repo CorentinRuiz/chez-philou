053b2465e99fc6b9e1aea4c776f620d9de6527c6
Thomas Guiot <thomas.guiot@etu.univ-cotedazur.fr>
2023-09-18 09:33:38 +0200

    Added file local-install-all.sh

---
+++ backend/local-install-all.sh
@@ -0,0 +1,19 @@
+echo "Install dining-service"
+cd ./dining-service/
+npm i
+
+echo "Install gateway-service"
+cd ../gateway/
+npm i
+
+echo "Install integration-tests"
+cd ../integration-tests/
+npm i
+
+echo "Install kitchen-service"
+cd ../kitchen-service/
+npm i
+
+echo "Install menu-service"
+cd ../menu-service/
+npm i
