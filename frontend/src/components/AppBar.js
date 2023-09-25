6e7e66cd1cacad10be877bb59fd567fc04e43b3b
Antony Martin <antony.mrtn06@gmail.com>
2023-09-25 09:46:33 +0200

    Fixing home page responsiveness issue

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -1,59 +1,45 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { Box, Grid } from "@mui/material";
-import { Divider, Typography, Button } from "antd";
+import {useLocation, useNavigate} from "react-router-dom";
+import {Box, Grid, Stack} from "@mui/material";
+import {Divider, Typography, Button} from "antd";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
-const { Title } = Typography;
+const {Title} = Typography;
 
 const AppBar = () => {
-  const location = useLocation();
-  const tableId = location.pathname.split("/takeOrder/")[1];
+    const location = useLocation();
+    const tableId = location.pathname.split("/takeOrder/")[1];
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div>
-      <Box sx={{ width: "100%" }}>
-        <Grid container alignItems="center" style={{ marginTop: 15 }}>
-          <Grid item xs={6}>
-            <Title style={{ marginTop: 0 }} level={3}>
-              {location.pathname === "/" ? "Table selection" : "Order"}
-            </Title>
-          </Grid>
-          <Grid item xs={6}>
-            {location.pathname.includes("/takeOrder") ? (
-              <Box display="flex" alignItems="center">
-                <Title
-                  style={{
-                    marginTop: 0,
-                    textAlign: "right",
-                    marginRight: "15px",
-                  }}
-                  level={5}
-                >
-                  Table n°{tableId}
-                </Title>
-                <Button
-                  onClick={() => {
-                    navigate("/");
-                  }}
-                  danger
-                  style={{ marginBottom: "0.5em" }}
-                >
-                  <HighlightOffIcon />
-                </Button>
-              </Box>
-            ) : (
-              ""
-            )}
-          </Grid>
-        </Grid>
-        <Divider style={{ margin: 0 }} />
-      </Box>
-      <Divider style={{ margin: 0 }} />
-    </div>
-  );
+    return (
+        <div>
+            <Box sx={{width: "100%"}}>
+                <Grid container alignItems="center" style={{marginTop: 15, paddingBottom: 10}}>
+                    <Grid item xs={6}>
+                        <Title style={{margin: 0}} level={3}>
+                            {location.pathname === "/" ? "Table selection" : "Order"}
+                        </Title>
+                    </Grid>
+                    <Grid item xs={6}>
+                        {location.pathname.includes("/takeOrder") ? (
+                            <Stack direction="row" alignItems="center" spacing={2} justifyContent="flex-end">
+                                <Title style={{textAlign: "right"}} level={5}>
+                                    Table n°{tableId}
+                                </Title>
+                                <Button onClick={() => navigate("/")} danger>
+                                    <HighlightOffIcon/>
+                                </Button>
+                            </Stack>
+                        ) : (
+                            ""
+                        )}
+                    </Grid>
+                </Grid>
+                <Divider style={{margin: 0}}/>
+            </Box>
+        </div>
+    );
 };
 
 export default AppBar;
